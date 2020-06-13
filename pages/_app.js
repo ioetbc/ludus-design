eabf57d0273bf29cc75c93c5ba1477f2af4c13cc
William Cole <williamcole@Williams-MacBook-Pro-3.local>
2020-06-13 11:18:32 +0100

    hmmdd

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import '../styles/global.scss';
 import { AnimatePresence } from 'framer-motion'
 export default function MyApp({ Component, pageProps, router }) {
     return (
-        <AnimatePresence exitBeforeEnter>
-            <Component {...pageProps} key={router.route} />        
-        </AnimatePresence>
+        <Component {...pageProps} key={router.route} />        
     )
 }
